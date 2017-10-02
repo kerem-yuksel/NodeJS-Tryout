ca754e9c95fa4029e348a156c07d29d3775d836a
Kerem Yüksel <kerem.yuksel@outlook.com>
2017-10-02 22:59:41 +0200

    Tried out filestream and installed express

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
 var http = require("http");
+var fs = require("fs");
 http.createServer(function(request, response) {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end("Hello World!");
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    fs.readFile("content.txt", function(err, data) {
+        if (err) {
+            console.log("Error! Code: " + err.code + " - Message: " + err.message);
+        }
+        
+        response.end(data.toString());
+    });
 }).listen(8081);
 
 console.log("Server is running and accessible via http://localhost:8081/");
